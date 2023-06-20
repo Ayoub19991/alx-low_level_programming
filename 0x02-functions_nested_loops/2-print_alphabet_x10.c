bf7756bfe5a9c5a5085779c9a60bfa53c271a8b0
Ayoub19991 <bob07475710@gmail.com>
2023-06-20 03:26:18 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,13 +7,14 @@
 void print_alphabet_x10(void)
 {
 	char charr;
-    	int i;
-    	for (i=1;i<=10;i++)
+	int i;
+
+	for (i=1;i<=10;i++)
 	{
-        	for (charr='a';charr<='z';charr++)
+		for (charr='a';charr<='z';charr++)
 		{
             		_putchar(charr);
         	}
         	_putchar('\n');
-    	}
-}        
+	}
+}
