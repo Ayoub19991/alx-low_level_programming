8e98bf7574df2a8316cb24ef24c9e219ed67033b
Ayoub19991 <bob07475710@gmail.com>
2023-06-20 02:23:44 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - print _putchar
+ * Return: 0
+ */
+int main(){
+    char c1,c2,c3,c4,c5,c6,c7,c8;
+        c1='_';
+        c2='p';
+        c3='u';
+        c4='t';
+        c5='c';
+        c6='h';
+        c7='a';
+        c8='r';
+        printf("%c%c%c%c%c%c%c%c",c1,c2,c3,c4,c5,c6,c7,c8);
+putchar('\n');
+
+return 0;
+}
