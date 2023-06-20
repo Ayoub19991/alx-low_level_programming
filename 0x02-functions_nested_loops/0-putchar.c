fb5725d4fd74f9c2136997fbddb9c57e9473aa8e
Ayoub19991 <bob07475710@gmail.com>
2023-06-20 02:38:18 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,20 @@
 #include "main.h"
 /**
  * main - print _putchar
- * is written by mbah
  * Return: 0
  */
+int main(){
+    char c1,c2,c3,c4,c5,c6,c7,c8;
+        c1='_';
+        c2='p';
+        c3='u';
+        c4='t';
+        c5='c';
+        c6='h';
+        c7='a';
+        c8='r';
+        printf("%c%c%c%c%c%c%c%c",c1,c2,c3,c4,c5,c6,c7,c8);
+putchar('\n');
 
-int main(void)
-{
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-return (0);
+return 0;
 }
-
