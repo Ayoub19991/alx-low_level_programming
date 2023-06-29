b5aca7ab14decad9962515aacb19a2e3e0108315
Ayoub19991 <bob07475710@gmail.com>
2023-06-29 13:13:51 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -8,10 +8,16 @@ int main(void)
 
   a[2] = 1024;
   p = &n;
-
-  /* Alternative solution using pointer arithmetic */
-  *((p - 10) + 12) = 98;
-
-  printf("a[2] = %d\n", a[2]);  /* This will print 98 */
-  return 0;
+  /*
+   * write your line of code here...
+   * Remember:
+   * - you are not allowed to use a
+   * - you are not allowed to modify p
+   * - only one statement
+   * - you are not allowed to code anything else than this line of code
+   */
+  *(p + 5) = 98;
+  /* ...so that this prints 98\n */
+  printf("a[2] = %d\n", a[2]);
+  return (0);
 }
